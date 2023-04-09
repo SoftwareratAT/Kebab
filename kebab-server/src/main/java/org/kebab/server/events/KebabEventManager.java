446a8a9154a2c06cc361144b6b8039407df073d9
Verklickt <verklickttwitch@gmx.de>
2023-04-09 15:10:46 +0200

    Added registration of eventmanager in KebabRegistry

--- kebab-server/src/main/java/org/kebab/server/events/KebabEventManager.java
+++ kebab-server/src/main/java/org/kebab/server/events/KebabEventManager.java
@@ -3,10 +3,15 @@ package org.kebab.server.events;
 import org.kebab.api.events.Event;
 import org.kebab.api.events.EventManager;
 import org.kebab.api.plugins.Plugin;
+import org.kebab.common.KebabRegistry;
 
 import java.util.concurrent.CompletableFuture;
 
 public class KebabEventManager implements EventManager {
+    public KebabEventManager() {
+        KebabRegistry.register(this);
+    }
+
     @Override
     public void registerListener(Plugin plugin, Object listener) {
 
