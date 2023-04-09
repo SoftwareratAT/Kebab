23f3d72084d02befa5691d83bb37db094985a897
Verklickt <verklickttwitch@gmx.de>
2023-04-09 15:12:45 +0200

    Added methods

--- kebab-api/src/main/java/org/kebab/api/entity/Player.java
+++ kebab-api/src/main/java/org/kebab/api/entity/Player.java
@@ -2,7 +2,9 @@ package org.kebab.api.entity;
 
 import net.kyori.adventure.text.Component;
 import org.kebab.api.commands.CommandSource;
+import org.kebab.api.packet.OutgoingPacket;
 
+import java.io.IOException;
 import java.util.UUID;
 
 public interface Player extends LivingEntity, CommandSource {
@@ -10,4 +12,7 @@ public interface Player extends LivingEntity, CommandSource {
     String getName();
     void disconnect(Component reason);
     boolean equals(Player player);
+    void sendPacket(OutgoingPacket packet) throws IOException;
+    void sendPluginMessage(String channel, byte[] message) throws IOException;
+    void sendServerBrand(Component brand);
 }
